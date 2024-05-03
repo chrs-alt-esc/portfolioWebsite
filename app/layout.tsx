f50337034618253bbb06b50b076ee779dd68e311
eschriscobar <chris.escobar12@gmail.com>
2024-05-03 00:36:18 -0500

    meta tag updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,15 +24,15 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/img/icon.ico" sizes="any" />
         <meta property="og:title" content="Chris | Dev Portfolio" />
-        <meta property="og:description" content="Software dev with 3 YOE"/>
-        <meta property="og:image" content="/img/meta-image.jpg" />
+        <meta property="og:description" content="Software developer with 3 years of experience. Currently on the market for a software developer position."/>
+        <meta property="og:image" content="/img/meta-img-1200x630.jpg" />
         <meta property="og:url" content="https://chrisesco.vercel.app/" />
         <meta property="og:type" content="website" />
         <meta name="twitter:title" content="Chris | Dev Portfolio" />
-        <meta name="twitter:description" content="Software dev with 3 YOE" />
-        <meta name="twitter:image" content="/img/meta-image.jpg" />
-        <meta name="twitter:card" content="/img/meta-image.jpg" />
-        <meta name="image" content="/img/meta-image.jpg"/>
+        <meta name="twitter:description" content="Software developer with 3 years of experience. Currently on the market for a software developer position." />
+        <meta name="twitter:image" content="/img/meta-img-1200x630.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="image" content="/img/meta-img-1200x630.jpg"/>
       </head>
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:-text-opacity-90`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
